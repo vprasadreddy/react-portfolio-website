3a9a960c92a678c0e19c278b290b761e15601273
vprasadreddy <prasad.reddy0708@gmail.com>
2022-01-07 21:28:37 -0500

    published portfolio page

---
+++ src/assests/projects.js
@@ -0,0 +1,56 @@
+import developer from "../assests/developer.png";
+import foodrecipeapp from "../assests/foodrecipeapp.png";
+import project from "../assests/project.png";
+import SPFxPnpCarousel from "../assests/SPFxPnpCarousel.png";
+import SPFxProjectManagement from "../assests/SPFxProjectManagement.png";
+
+export const projects = [
+  {
+    name: " Recipe Search Application using React",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    image: require("../assests/foodrecipeapp.png"),
+    applicationurl: "https://react-recipe-search-c90d0.firebaseapp.com",
+    githuburl: "https://github.com/vprasadreddy/react-recipe-search",
+  },
+  {
+    name: "CRUD Application using React",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    image: require("../assests/project.png"),
+    applicationurl: "",
+    githuburl: "",
+  },
+  {
+    name: "React Application",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    image: require("../assests/project.png"),
+    applicationurl: "",
+    githuburl: "",
+  },
+  {
+    name: "SPFx PnP Carousel",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    image: require("../assests/SPFxPnpCarousel.png"),
+    applicationurl:
+      "https://github.com/vprasadreddy/spfx-pnpcarousel-with-react",
+    githuburl: "https://github.com/vprasadreddy/spfx-pnpcarousel-with-react",
+  },
+  {
+    name: "ss",
+    description: "",
+    image: require("../assests/project.png"),
+    applicationurl: "",
+    githuburl: "",
+  },
+  {
+    name: "SPFx Project Management Application",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    image: require("../assests/SPFxProjectManagement.png"),
+    applicationurl: "https://github.com/vprasadreddy/spfx-project-management",
+    githuburl: "https://github.com/vprasadreddy/spfx-project-management",
+  },
+];
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,7 @@ import project from "../assests/project.png";
 import SPFxPnpCarousel from "../assests/SPFxPnpCarousel.png";
 import SPFxProjectManagement from "../assests/SPFxProjectManagement.png";
 import Typed from "react-typed";
+import { projects } from "../assests/projects";
 
 function Projects() {
   return (
@@ -13,174 +14,41 @@ function Projects() {
         <h3 className="text-center pt-5">Projects</h3>
         <div className="projects2-container mt-3">
           <div className="row pb-3 projects-row">
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 d-flex justify-content-center">
-              <div
-                className="card m-2"
-                style={{ width: "18rem", maxWidth: "18rem" }}
-              >
-                <img
-                  className="card-img-top"
-                  src={foodrecipeapp}
-                  alt="Card image cap"
-                  style={{ height: "13rem" }}
-                />
-                <div className="card-body">
-                  <h6 class="card-title">
-                    Recipe Search Application using React
-                  </h6>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <a
-                    href="https://react-recipe-search-c90d0.firebaseapp.com/"
-                    className="card-link"
-                    target="_blank"
+            {projects.map((project) => {
+              return (
+                <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 d-flex justify-content-center">
+                  <div
+                    className="card m-2"
+                    style={{ width: "18rem", maxWidth: "18rem" }}
                   >
-                    View App
-                  </a>
-                  <a
-                    href="https://github.com/vprasadreddy/react-recipe-search"
-                    className="card-link"
-                    target="_blank"
-                  >
-                    Github Link
-                  </a>
+                    <img
+                      className="card-img-top"
+                      src={project.image}
+                      alt="Card image cap"
+                      style={{ height: "13rem" }}
+                    />
+                    <div className="card-body">
+                      <h6 class="card-title">{project.name}</h6>
+                      <p className="card-text">{project.description}</p>
+                      <a
+                        href={project.applicationurl}
+                        className="card-link"
+                        target="_blank"
+                      >
+                        View App
+                      </a>
+                      <a
+                        href={project.githuburl}
+                        className="card-link"
+                        target="_blank"
+                      >
+                        Github Link
+                      </a>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 d-flex justify-content-center">
-              <div className="card m-2" style={{ width: "18rem" }}>
-                <img
-                  className="card-img-top"
-                  src={project}
-                  alt="Card image cap"
-                  style={{ height: "13rem" }}
-                />
-                <div className="card-body">
-                  <h6 class="card-title">CRUD Application using React</h6>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <a href="#" className="card-link">
-                    View Project
-                  </a>
-                  <a href="" className="card-link" target="_blank">
-                    Github Link
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 d-flex justify-content-center">
-              <div className="card m-2" style={{ width: "18rem" }}>
-                <img
-                  className="card-img-top"
-                  src={project}
-                  alt="Card image cap"
-                />
-                <div className="card-body">
-                  <h6 class="card-title">React Application</h6>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <a href="#" className="card-link">
-                    View Project
-                  </a>
-                  <a href="" className="card-link" target="_blank">
-                    Github Link
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 d-flex justify-content-center">
-              <div className="card m-2" style={{ width: "18rem" }}>
-                <img
-                  className="card-img-top"
-                  src={SPFxPnpCarousel}
-                  alt="Card image cap"
-                  style={{ height: "13rem" }}
-                />
-                <div className="card-body">
-                  <h6 class="card-title">SPFx PnP Carousel</h6>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <a
-                    href="https://github.com/vprasadreddy/spfx-pnpcarousel-with-react"
-                    className="card-link"
-                  >
-                    View Project
-                  </a>
-                  <a
-                    href="https://github.com/vprasadreddy/spfx-pnpcarousel-with-react"
-                    className="card-link"
-                    target="_blank"
-                  >
-                    Github Link
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 d-flex justify-content-center">
-              <div className="card m-2" style={{ width: "18rem" }}>
-                <img
-                  className="card-img-top"
-                  src={SPFxProjectManagement}
-                  alt="Card image cap"
-                  style={{ height: "13rem" }}
-                />
-                <div className="card-body">
-                  <h6 class="card-title">
-                    SPFx Project Management Application
-                  </h6>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <a
-                    href="https://github.com/vprasadreddy/spfx-project-management"
-                    className="card-link"
-                  >
-                    View Project
-                  </a>
-                  <a
-                    href="https://github.com/vprasadreddy/spfx-project-management"
-                    className="card-link"
-                    target="_blank"
-                  >
-                    Github Link
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 d-flex justify-content-center">
-              <div className="card m-2" style={{ width: "18rem" }}>
-                <img
-                  className="card-img-top"
-                  src={project}
-                  alt="Card image cap"
-                  style={{ height: "13rem" }}
-                />
-                <div className="card-body">
-                  <h6 class="card-title">
-                    Recipe Search Application using React
-                  </h6>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <a href="#" className="card-link">
-                    View Project
-                  </a>
-                  <a href="" className="card-link" target="_blank">
-                    Github Link
-                  </a>
-                </div>
-              </div>
-            </div>
+              );
+            })}
           </div>
         </div>
       </section>
